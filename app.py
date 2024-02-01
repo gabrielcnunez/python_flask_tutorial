584f29cce4ad51d3ccde42ab02204dc306a3a0ac
Gabe Nunez <gabriel.c.nunez@gmail.com>
2024-01-31 20:19:24 -0700

    Create get people endpoint

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import render_template
+import connexion
 
-app = Flask(__name__)
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000)
---
+++ people.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+PEOPLE = {
+    "Fairy": {
+        "fname": "Tooth",
+        "lname": "Fairy",
+        "timestamp": get_timestamp(),
+    },
+    "Ruprecht": {
+        "fname": "Knecht",
+        "lname": "Ruprecht",
+        "timestamp": get_timestamp(),
+    },
+    "Bunny": {
+        "fname": "Easter",
+        "lname": "Bunny",
+        "timestamp": get_timestamp(),
+    }
+}
+
+def read_all():
+    return list(PEOPLE.values())
